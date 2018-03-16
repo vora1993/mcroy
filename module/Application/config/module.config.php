8691f0805f7e1b70a7627fde01de625ed6cb816f
nusdon <don@nustechnology.com>
2018-03-16 14:53:16 +0700

    Update partial for page apply

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -138,6 +138,7 @@ return array(
             'last_news'                => __DIR__ . '/../view/partial/last-news.phtml',
             'credit_card_sliders'   => __DIR__ . '/../view/partial/credit-card-sliders.phtml',
             'testimonials'                => __DIR__ . '/../view/partial/testimonials.phtml',
+            'action_cart_benefit'                => __DIR__ . '/../view/partial/action-cart-benefit.phtml'
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
